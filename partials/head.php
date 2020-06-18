435abd94a20fe2da083b6c35e18a5c9283c582e9
locness3 <locness3@orange.fr>
2020-06-18 13:19:24 +0200

    Add charset & viewport meta + actually remove the bg image on login <840 px

--- partials/head.php
+++ partials/head.php
@@ -1,3 +1,6 @@
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
 <link rel="stylesheet" href="/assets/css/normalize.css">
 <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet">
 <link rel="stylesheet" href="/assets/fonts/fonts.css">
