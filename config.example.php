6ee2d386cc4909c1372579e73ccd5aa143eaf8da
locness3 <locness3@orange.fr>
2020-06-20 08:39:51 +0200

    Load the DB credentials from a config.php file, untracked by git
    (should be copied from config.example.php)

---
+++ config.example.php
@@ -0,0 +1,6 @@
+<?php
+
+define("DB_HOST", "localhost");
+define("DB_USER", "zytixon");
+define("DB_PASSWORD", "discordisstillbetter");
+define("DB_NAME", "zytixon");
--- lib/boot.php
+++ lib/boot.php
@@ -5,10 +5,11 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
 require __DIR__ . "/functions.php";
+require __DIR__ . "/../config.php";
 
 session_start();
 
-$database = new mysqli("localhost", "zytixon", "discordisstillbetter", "zytixon");
+$database = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
 
 if ($database->connect_error) {
     die("<h1>An error occured connecting to the database. That's sad.</h1><br>" . $database->connect_error);
